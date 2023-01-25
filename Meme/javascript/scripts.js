e9956c8c033ddca4b8765e908b42a3a25c5ef36c
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-24 21:09:32 -0300

    Image mapping

--- Meme/javascript/scripts.js
+++ Meme/javascript/scripts.js
@@ -1,9 +1,34 @@
 function mapImageList(){
     const memesObject = [
         {
-            "name":
-        }
-    ] 
+            "name": "chapolin",
+            "path": "./pictures/chapolin.jpg"
+        },
+        {
+            "name": "chloe",
+            "path": "./pictures/chloe.jpg"
+        },
+        {
+            "name": "computer-reaction",
+            "path": "pictures/computer-reaction.jpg"
+        },
+        {
+            "name": "funny-cat1",
+            "path": "pictures/funny-cat1.png"
+        },
+        {
+            "name": "funny-cat2",
+            "path": "pictures/funny-cat2.png"
+        },
+        {
+            "name": "like-a-boss",
+            "path": "pictures/like-a-boss.png"
+        },
+        {
+            "name": "wtf",
+            "path": "./pictures/wtf.jpg"
+        },
+    ]
 }
 
 function createGallery(){
